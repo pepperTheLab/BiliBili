8e987d4c0e240b996e73858b34ae202ed72d3553
Xinning Wang <xinningwang1031@gwu.edu>
2019-12-15 21:11:24 -0500

    4 - variables

---
+++ Python_Basics/4_variables/variables.py
@@ -0,0 +1,43 @@
+# -*- coding: utf-8 -*-
+
+# Integer
+a = 1
+
+# Float
+b = 1.0
+c = 1.0/2.0
+
+# String
+d = 'Anthony'
+
+# List (mutable)
+e = [1, 2, 3, 4, 5]
+e[1:]
+f = ['Wang', 25, 'China']
+e[0] = 10
+e
+
+# Tuple (imutable)
+g = (1, 2, 3, 4, 5)
+g[2]
+g[1:]
+g[0] = 10
+g = list(g)
+g[0] = 10
+g = tuple(g)
+
+# Dictionary
+f[2]
+h = {
+     'name': 'Wang',
+     'age': 25,
+     'country': 'China'
+     }
+h['country']
+
+# Set
+i = {1, 2, 3, 4, 5}
+j = [1, 1, 2, 2, 3, 4, 5]
+j = list(set(j))
+
+
