add5452fccb3cd468788ffe2189f7b2a94f10cc4
Xinning Wang <xinningwang1031@gwu.edu>
2019-12-18 21:16:51 -0500

    5/6 loops

---
+++ Python_Basics/5_while_loop/while_loop.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+
+result = 1 + 2 + 3 + 4 + 5 + 6 + 7 + 8 + 9 + 10
+
+
+value = 1
+result = 0
+
+while (value <= 10):
+    print(f'Result: {result}, Value: {value}')
+    result = result + value
+    value = value + 1
+
---
+++ Python_Basics/6_for_loop/for_loop.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+
+sample_list = [4, 6, 19, 72, 53, 6, 7, 14, 77, 92, 56]
+
+#i = 0
+#new_list = []
+#while i < length:
+#    new_list.append(sample_list[i])
+#    i = i + 1
+    
+new_list = []
+for element in sample_list:
+    new_list.append(element ** 2)
+
